3fd80f52937df959fa7fa1f35a0c2bb8c3ba0d98
Ramaswamy <srramas@acbc32c3dd6d.ant.amazon.com>
2017-12-05 19:41:20 +1100

    change

---
+++ src/main/java/com/emp/LoginDemoApplication.java
@@ -0,0 +1,21 @@
+package com.emp;
+
+import java.io.IOException;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.*;
+import javafx.stage.Stage;
+
+/** Main application class for the login demo application */
+public class LoginDemoApplication extends Application {
+  public static void main(String[] args) { launch(args); }
+  @Override public void start(Stage stage) throws IOException {
+    Scene scene = new Scene(new StackPane());
+    
+    LoginManager loginManager = new LoginManager(scene);
+    loginManager.showLoginScreen();
+
+    stage.setScene(scene);
+    stage.show();
+  }
+}
